86d1c37213325b43e5b551c5d9d7deb07ea82503
happyCoder <744209932@qq.com>
2024-05-13 22:09:29 +0800

    fix：css配置做的处理

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ import { count } from "./counter.js";
 
 import _ from "lodash";
 
+// import IndexCss from "./index.module.css";
+
+// console.log("IndexCss:", IndexCss);
+
+import importACss from "./importA.module.css";
+
+console.log("importACss", importACss);
+
+const div = document.createElement("div");
+
+document.body.appendChild(div);
+
+div.className = importACss["footerContent"];
+
 // console.log("count", count);
 
 console.log("import.meta.env:", import.meta.env);
--- vite.base.config.js
+++ vite.base.config.js
@@ -2,4 +2,25 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   envPrefix: "ENV_",
+  css: {
+    // css 配置
+    modules: {
+      // 覆盖默认的
+      localsConvention: "camelCaseOnly", // 修改生成的配置对象的key的展现形式（驼峰还是中划线形式）
+      //   generateScopedName: "[name]_[module]_[hash:5]", // 配置生成的文件名称
+      // 也可以使用函数形式处理
+      generateScopedName: (name, filename, css) => {
+        /**
+         * name: class 文件名称
+         * filename：当前文件的路径
+         * css：class里面定义的属性
+         */
+        console.log(name, filename, css);
+        return `${name}_${Math.random().toString(36).substr(5, 10)}`;
+      },
+      scopeBehaviour: "local",
+      hashPrefix: "hello", // 生成hash值的时候，会把自定义的这个hello也会包含进去
+      globalModulePaths: ["./importA.module.css"], // 代表不想参与到css模块化路径中
+    },
+  },
 });
