5094f3bfbbdbe74949a885388f2ba93361a0889f
happyCoder <744209932@qq.com>
2024-05-13 09:41:57 +0800

    fix：vite 环境变量配置

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { count } from "./counter.js";
 
 import _ from "lodash";
 
-console.log("count", count);
+// console.log("count", count);
+
+console.log("import.meta.env:", import.meta.env);
 
 console.log(1233);
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import baseConfig from "./vite.base.config";
 import devConfig from "./vite.dev.config";
@@ -17,7 +17,21 @@ const envConfig = {
   },
 };
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
   console.log("command", command);
+  // 获取 env
+  /**
+   * mode：执行命令是npm run dev --mode development，代表dev环境
+   * process.cwd(): 获取当前的目录
+   * 第三个参数：指定读取那个env文件,默认为.env
+   */
+  // vite.config.js 会先读取.env文件，解析配置并把内容放到一个对象里
+  /**
+   * 若是执行的是dev环境，则会在把.env.development配置里的内容解析出来，放到对象中
+   * 若是执行的是build环境，则会在把.env.production配置里的内容解析出来，放到对象中
+   * 此对象里的内容会覆盖掉.env里获取到的对象内容
+   */
+  const env = loadEnv(mode, process.cwd(), "");
+  //   console.log("env:", env);
   return envConfig[command]();
 });
